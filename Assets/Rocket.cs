0e6380ff63c143668a3bc5139daff8b1aa4fb584
Victor Miguel <70338725+vtr363@users.noreply.github.com>
2021-02-07 13:31:34 -0300

    ship movement

---
+++ Assets/Rocket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)){
+            //thrusting
+            print("thrusting");
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            //rotate left
+            print("rotate left");
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            //rotate right
+            print("rotate right");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
